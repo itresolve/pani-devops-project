2c67285d46dff9dbf809358901ef8e83ccadac9d
itresolve <54575354+itresolve@users.noreply.github.com>
2022-02-09 18:35:13 -0500

    update

--- vagrant/Automated_provisioning/nginx.sh
+++ vagrant/Automated_provisioning/nginx.sh
@@ -1,7 +1,7 @@
 # adding repository and installing nginx		
 apt update
 apt install nginx -y
-cat <<EOT > /etc/nginx/sites-available/vproapp
+cat <<EOT > vproapp
 upstream vproapp {
 
  server app01:8080;
